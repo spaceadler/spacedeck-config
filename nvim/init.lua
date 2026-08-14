43c49a9409f393f46630a6d2e29fc83f3900caf1
spaceadler <host.directly413@aleeas.com>
2026-08-14 20:44:32 +0300

    feat: added a lot of stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -155,7 +155,7 @@ do
   --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
   --   See `:help lua-options`
   --   and `:help lua-guide-options`
-  vim.o.list = true
+  vim.o.list = false
   vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
   -- Preview substitutions live, as you type!
